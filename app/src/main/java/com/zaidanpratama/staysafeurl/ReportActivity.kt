3c64864dcad6ba0573663c189eeec139eee6e819
Zaidan Pratama <zaidanpratamaa@gmail.com>
2021-12-09 20:30:44 +0700

    add report, result, and list item view

---
+++ app/src/main/java/com/zaidanpratama/staysafeurl/ReportActivity.kt
@@ -0,0 +1,11 @@
+package com.zaidanpratama.staysafeurl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
